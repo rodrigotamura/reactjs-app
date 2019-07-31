91353c0cfc386cbbf3443252e9543bb0ef6cc211
Rodrigo Tamura <ti.tamura@restauramundo.com>
2019-07-31 19:53:38 -0300

    Product detail page and stylization done.

--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+import api from '../../services/api';
+
+import './styles.css';
 
 export default class Product extends Component {
+    state = {
+        product: {}
+    };
+
+    async componentDidMount(){
+        // obtendo a ID
+        const { id } = this.props.match.params; // pega o 5 de /products/5
+
+        const response = await api.get(`/products/${id}`);
+
+        this.setState({ product: response.data });
+    }
+
     render() {
+        const { product } = this.state; // pra nao ter que ficar repetindo this.state.product
+
         return (
-            <h1>Product</h1>
+            <div className="product-info">
+                <h1>{product.title}</h1>
+                <p>{product.description}</p>
+                <p>
+                    URL: <a href={product.url}>{product.url}</a>
+                </p>
+            </div>
         );
     }
 }
